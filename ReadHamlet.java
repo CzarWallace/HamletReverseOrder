b8c18449a7b63d441e47c349e9a567db3aa1b15a
CzarWallace <111289427+CzarWallace@users.noreply.github.com>
2022-11-11 02:58:20 -0500

    Add files via upload

--- ReadHamlet.java
+++ ReadHamlet.java
@@ -9,32 +9,45 @@ import java.io.FileNotFoundException;
 
     public class ReadHamlet {
         public static void main(String[] args) {
+
             try {
-                File file = new File("src/FileIODemo/hamlet.txt");
+                File file = new File("src/AssignmentHamlet/hamlet.txt");
+
 
                 try (Scanner scanner = new Scanner(file);) {
                     while (scanner.hasNextLine()) {
                         String line = scanner.nextLine();
                         System.out.println(line);
+
+
                     }
+
                 }
+                String output = reverse("src/AssignmentHamlet/hamlet.txt");
+                System.out.println(output);
+
+
             }catch(FileNotFoundException e){
                 System.out.println("Error. ");
                 e.printStackTrace();
             }
-
-            System.out.println(HamletReverseOrder(Reverse));
         }
 
 
         //(2) reverse order
 
-        public  ReadHamlet HamletInReverseOrder(String Reverse){
-            String  output = new ReadHamlet();
-            for(Integer element: Reverse){
-                output.add(element);
+        public static String reverse (String reverse){
+            char []  str = new char[reverse.length()];
+            int strIndex = 0;
+            for(int i = reverse.length()-1; i >= 0; i--){
+                str [strIndex] = reverse.charAt(i);
+                strIndex++;
             }
-            return output;
+            String reversed = "";
+            for(int i = 0; i < reversed.length(); i++){
+                reverse = reverse + str[i];
+            }
+            return reverse;
         }
         }
 
